85b6fddc666c980cb3a607ab8919743be2a9d307
Kristian Frølund Jørgensen <kristian@tlt.local>
2015-06-02 08:16:10 +0400

    Added support for right aligned languages.

--- JSQMessagesViewController/Controllers/JSQMessagesViewController.h
+++ JSQMessagesViewController/Controllers/JSQMessagesViewController.h
@@ -268,4 +268,12 @@
  */
 - (void)scrollToBottomAnimated:(BOOL)animated;
 
+/**
+ *  Determines the alignment of the cell. Given a text, it tries to guess the language, and determine the alignment from that guess. 
+ *  Default alignment is @link NSTextAlignmentLeft
+ *
+ *  @param astring is the text, from which the alingment should be determined.
+ */
+- (NSTextAlignment)alignmentForString:(NSString *)astring;
+
 @end
--- JSQMessagesViewController/Controllers/JSQMessagesViewController.m
+++ JSQMessagesViewController/Controllers/JSQMessagesViewController.m
@@ -468,6 +468,14 @@ static void * kJSQMessagesKeyValueObservingContext = &kJSQMessagesKeyValueObserv
     if (!isMediaMessage) {
         cell.textView.text = [messageItem text];
 
+        // Auto detect language and set text alignment accordingly
+        NSTextAlignment ta = NSTextAlignmentLeft;
+        if (cell.textView.text != nil)
+            ta = [self alignmentForString:cell.textView.text];
+        else if (cell.textView.attributedText != nil)
+            ta = [self alignmentForString:cell.textView.attributedText.string];
+        cell.textView.textAlignment = ta;
+        
         if ([UIDevice jsq_isCurrentDeviceBeforeiOS8]) {
             //  workaround for iOS 7 textView data detectors bug
             cell.textView.text = nil;
@@ -534,6 +542,18 @@ static void * kJSQMessagesKeyValueObservingContext = &kJSQMessagesKeyValueObserv
     cell.layer.shouldRasterize = YES;
 
     return cell;
+}
+
+-(NSTextAlignment)alignmentForString:(NSString *)astring
+{
+    if (astring.length) {
+        NSArray *rightLeftLanguages = @[@"ar",@"arc",@"bcc",@"bqi",@"ckb",@"dv",@"fa",@"glk",@"he",@"ku",@"mzn",@"pnb",@"ps",@"sd",@"ug",@"ur",@"yi"];
+        NSString *lang = CFBridgingRelease(CFStringTokenizerCopyBestStringLanguage((CFStringRef)astring,CFRangeMake(0,[astring length])));
+        if ([rightLeftLanguages containsObject:lang]) {
+            return NSTextAlignmentRight;
+        }
+    }
+    return NSTextAlignmentLeft;
 }
 
 - (UICollectionReusableView *)collectionView:(JSQMessagesCollectionView *)collectionView
